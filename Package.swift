c57657e9acb941cca71fa68c9a9c461091c6538a
Qithking <tanklzs@163.com>
2026-04-15 21:26:45 +0800

    fix: 更新 Package.swift 配置

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
             targets: ["SwallowScreen"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections", from: "1.1.0")
+    ],
     targets: [
         .executableTarget(
             name: "SwallowScreen",
