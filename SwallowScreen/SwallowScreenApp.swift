3eb8bb4a6583f30c0492cb4d8d979532d842b862
Qithking <tanklzs@163.com>
2026-04-15 13:37:06 +0800

    Initial Commit

---
+++ SwallowScreen/SwallowScreenApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwallowScreenApp.swift
+//  SwallowScreen
+//
+//  Created by thking on 2026/4/15.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwallowScreenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
